28303e5faccba7c346e775ced98775d2b86e0cb6
Christerper <christerper91@gmail.com>
2021-04-07 23:08:41 +0800

    adjust 7 attempt sudenly difrent output

--- hello-world.rb
+++ hello-world.rb
@@ -1,40 +1,38 @@
 system("cls") || system("clear")
 
 loop do
-  leaderboard = []
-  player = []
+
+ scores = {}
+ leaderboard = {}
   puts "Welcome Aboard Player".upcase
   #input player name
   puts "Please Enter your name "
-  name = Array.new
   name = gets.chomp
 
   #Guess from Player - number only
   puts "Welcome #{name}!"
   age = rand (1..10)
-  tries = 7
   puts "Guess a number! Im thingking between 1 - 10, take a guess :)"
   puts "Answer Is = #{age}"
   guess = gets.chomp
 
+
    if guess.to_i == age
      puts "congratulation!! #{name}. Your are correct"
-   else puts "Best of Luck! try again! "
+   else puts "Sory Try Again! "
    end
 
  #while loop
  attempt = 1
- while guess.to_i !=age && (attempt < 7)
+ while guess.to_i != age && (attempt < 7)
  attempt +=1
- guess = gets.chomp
- if guess.to_i == age
-   puts "congratulation!! #{name}. Your are correct"
- else puts "Best of Luck! try again! "
+ puts "No Luck! Try Again! "
+   guess = gets.chomp
  end
 
   if guess.to_i == age && (attempt < 7)
   puts "congratulation!! #{name}. Your are correct"
-  else puts "sorry have run out of attempt :/ "
+else puts "sorry you have run out of attempt :/ "
   end
 
 puts "="*100
@@ -42,6 +40,5 @@ puts "="*100
 
 
   puts "List Of Player"
-  puts name
+  
   end
-end
