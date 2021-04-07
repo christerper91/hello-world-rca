644137935a75d3b42d45764e0ecef16d32988619
Christerper <christerper91@gmail.com>
2021-04-07 13:41:58 +0800

    add player list name

--- hello-world.rb
+++ hello-world.rb
@@ -1,20 +1,21 @@
 loop do
-
-  puts "Hello World".upcase
+  leaderboard = []
+  player = []
+  puts "Welcome Aboard Player".upcase
   #input player name
-  print "What's your name? "
-  first_name = gets.chomp
-  first_name.capitalize!
+  puts "Please Enter your name "
+  name = Array.new
+  name = gets.chomp
 
   #Guess from Player - number only
-  print "Welcome #{first_name}!"
-  age = 30
+  puts "Welcome #{name}!"
+  age = rand (1..10)
   tries = 7
-  puts "Guess my Age! Im thingking between 20 - 30, take a guess :)"
+  puts "Guess a number! Im thingking between 1 - 10, take a guess :)"
   guess = gets.chomp
 
    if guess.to_i == age
-     puts "congratulation!! #{first_name}. Your are correct"
+     puts "congratulation!! #{name}. Your are correct"
    else puts "Best of Luck! try again! "
    end
 
@@ -24,13 +25,20 @@ loop do
  attempt +=1
  guess = gets.chomp
  if guess.to_i == age
-   puts "congratulation!! #{first_name}. Your are correct"
+   puts "congratulation!! #{name}. Your are correct"
  else puts "Best of Luck! try again! "
  end
  end
 
   if guess.to_i == age && (attempt < 7)
-  puts "congratulation!! #{first_name}. Your are correct"
+  puts "congratulation!! #{name}. Your are correct"
   else puts "sorry have run out of attempt :/ "
   end
-end
+
+puts "="*100
+puts "="*100
+
+
+  puts "List Of Player"
+  puts name
+  end
