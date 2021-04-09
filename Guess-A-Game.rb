6d803c3dd7122e079add0f48d2bfc42c03e8cd9d
Christerper <christerper91@gmail.com>
2021-04-09 14:17:47 +0800

    improve selection pages loop

--- Guess-A-Game.rb
+++ Guess-A-Game.rb
@@ -16,17 +16,19 @@ end
 
 def play_game(name, scores)
   system("cls") || system("clear")
+  puts "="*100
   puts "Lets Guess A Number Between 1 - 10"
+  puts "="*100
 
   age = rand (1..10)
-  puts "Correct Answer Is = #{age}"
+  puts "Correct Answer Is = #{age}, Enter Your Guess #{name}"
   guess = gets.chomp
   attempt = 1
   tries = 7
   scores = {}
   if guess.to_i == age
     scores[name]=(tries-attempt)
-    puts "congratulation!! #{name}. Your are correct"
+    puts "Congratulation!! #{name}. You are correct"
     show_leaderboard(name, scores)
   else
     puts "Sory Try Again! Attempt left"
@@ -43,7 +45,7 @@ def play_game(name, scores)
   if guess.to_i == age && (attempt < 7)
     scores[name]=(tries-attempt)
     puts "="*100
-    puts "congratulation!! #{name}. Your are correct"
+    puts "Congratulation!! #{name}. You are correct"
     puts "="*100
     puts "Press 'n' if you want continue as a new player"
     puts "Press 'c'if you want continue as same player"
@@ -77,25 +79,28 @@ def show_leaderboard(name, scores)
 end
 
 def print_menu(name, scores)
-  puts "="*100
-  puts "1. Play Game"
-  puts "2. Show Leaderboard"
-  puts "3. Print Menu"
-  puts "="*100
-  option = gets.chomp
-  if option.to_i == 1
-    play_game(name, scores)
-  elsif option.to_i == 2
-    show_leaderboard(name, scores)
-  elsif option.to_i == 3
-    system("cls") || system("clear")
+  loop do
     puts "="*100
-    puts "Welcome Aboard #{name}, Select Your Option 1 - 3"
-    print_menu(name, scores)
-  else
-    puts "Sory Wrong Input, Choose Option 1, 2, 3 ONLY!"
-  end
+    puts "1. Play Game"
+    puts "2. Show Leaderboard"
+    puts "3. Print Menu"
+    puts "="*100
+    option = gets.chomp
 
+    if option.to_i == 1
+      play_game(name, scores)
+    elsif option.to_i == 2
+      show_leaderboard(name, scores)
+    elsif option.to_i == 3
+      system("cls") || system("clear")
+      puts "="*100
+      puts "Welcome Aboard #{name}, Select Your Option 1 - 3"
+      print_menu(name, scores)
+    else
+      system("cls") || system("clear")
+      puts "Sory Wrong Input #{name}, Choose Option 1, 2, 3 ONLY!"
+    end
+  end
 end
 
 def separator
